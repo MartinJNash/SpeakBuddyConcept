ba725b5abc96d72a882c8e43cc0f10140a9fcf0e
Martin Nash <martinnash@microsoft.com>
2024-02-13 11:27:19 -0800

    Initial Commit

---
+++ SpeakBuddyAssessment/SpeakBuddyAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakBuddyAssessmentApp.swift
+//  SpeakBuddyAssessment
+//
+//  Created by Martin Nash on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeakBuddyAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
