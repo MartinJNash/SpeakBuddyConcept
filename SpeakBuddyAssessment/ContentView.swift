0d2e7e9201aa3fd0df70eb3c8cd81d5d9e98b364
Martin Nash <martin.j.nash@gmail.com>
2024-02-15 01:13:24 -0800

    Fixed some colors

--- SpeakBuddyAssessment/ContentView.swift
+++ SpeakBuddyAssessment/ContentView.swift
@@ -14,8 +14,8 @@ struct BasicTheme {
     let textBaseColor = Color(red: 50/255.0, green: 53/255.0, blue: 55/255.0)
 
     let backgroundPurple = Color(red: 213/255.0, green: 210/255.0, blue: 1.0)
-    let barTop = Color(red: 88/255, green: 19/2552, blue: 1.0)
-    let barBottom = Color(red: 34/255.0, green: 14/255.0, blue: 1.0)
+    let barTop = Color(red: 88/255, green: 192/255, blue: 1.0)
+    let barBottom = Color(red: 31/255.0, green: 143/255.0, blue: 1.0)
     let ctaBlue = Color(red: 59/255.0, green: 167/255.0, blue: 1.0)
 
     let textGradientTop = Color(red: 101/255.0, green: 203/255.0, blue: 1.0)
