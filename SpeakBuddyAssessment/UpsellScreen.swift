9a57da3d01a2fb667bd9c30c3e84327cf7ad0f5e
Martin Nash <martin.j.nash@gmail.com>
2024-02-15 11:43:11 -0800

    Extracted BasicTheme to its own file

--- SpeakBuddyAssessment/UpsellScreen.swift
+++ SpeakBuddyAssessment/UpsellScreen.swift
@@ -1,21 +1,5 @@
 import SwiftUI
 
-struct BasicTheme {
-
-    static let shared = BasicTheme()
-
-    let textBaseColor = Color(red: 50/255.0, green: 53/255.0, blue: 55/255.0)
-
-    let backgroundPurple = Color(red: 213/255.0, green: 210/255.0, blue: 1.0)
-    let barTop = Color(red: 88/255, green: 192/255, blue: 1.0)
-    let barBottom = Color(red: 31/255.0, green: 143/255.0, blue: 1.0)
-    let ctaBlue = Color(red: 59/255.0, green: 167/255.0, blue: 1.0)
-
-    let textGradientTop = Color(red: 101/255.0, green: 203/255.0, blue: 1.0)
-    let textGradientBottom = Color(red: 27/255.0, green: 140/255.0, blue: 1.0)
-
-}
-
 struct UpsellScreen: View {
     var body: some View {
         ZStack(content: {
